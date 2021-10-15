660776d4044972e51e7082c25e54e3c54224ed51
Adrian Montero <abmontero@yahoo.com>
2021-10-15 16:36:39 -0600

    Se implemento Add-To-Cart desde product detail

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import NextLink from "next/link";
 import Image from "next/image";
 import {
@@ -14,14 +14,27 @@ import Layout from "../../components/Layout";
 import useStyles from "../../utils/styles";
 import db from "../../utils/database";
 import Product from "../../models/Product";
+import axios from "axios";
+import { Store } from "../../utils/Store";
 
 export default function ProductDetail(props) {
+  const { dispatch } = useContext(Store);
   const { product } = props;
   const classes = useStyles();
 
   if (!product) {
     return <div>Producto No Encontrado</div>;
   }
+
+  const addToCartHandler = async () => {
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock <= 0) {
+      window.alert("Sorry. Product is out of stock");
+      return;
+    }
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity: 1 } });
+  };
+
   return (
     <Layout title={product.name} description={product.description}>
       <div className={classes.section}>
@@ -90,7 +103,12 @@ export default function ProductDetail(props) {
                 </Grid>
               </ListItem>
               <ListItem>
-                <Button fullWidth variant="contained" color="primary">
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  onClick={addToCartHandler}
+                >
                   Add to cart
                 </Button>
               </ListItem>
